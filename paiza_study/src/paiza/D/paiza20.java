7a0d9d92f49ec643ce4619a14454b8915915e6e8
“shostitch“ <stitchsho0325@gmail.com>
2022-09-06 20:43:16 +0900

    20finish

---
+++ paiza_study/src/paiza/D/paiza20.java
@@ -0,0 +1,21 @@
+package paiza.D;
+
+import java.util.Scanner;
+
+public class paiza20 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int nums[] = new int[2];
+		int count = 0;
+
+		for(int i = 0; i < nums.length; i++) {
+			nums[i] = sc.nextInt();
+		}
+		sc.close();
+		while(nums[0] <= nums[1]) {
+			nums[0] *= 2;
+			count ++;
+		}
+		System.out.println(count);
+	}
+}
