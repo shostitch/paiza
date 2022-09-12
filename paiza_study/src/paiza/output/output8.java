f451e2157f6b20de7bfdc204e7d9fa6a811dfdf7
“shostitch“ <stitchsho0325@gmail.com>
2022-09-12 22:59:17 +0900

    今日学んだこと

--- paiza_study/src/paiza/output/output8.java
+++ paiza_study/src/paiza/output/output8.java
@@ -1,9 +1,18 @@
 package paiza.output;
 
+import java.util.Scanner;
+
 public class output8 {
 	  public static void main(String[] args) {
-//	    String str = "こんにちは、世界！";
-//	    System.out.println(str.charAt(3));  // 結果：ち
-//	  }
+	        Scanner sc = new Scanner(System.in);
+	        String N = sc.next();
+	        for(int i = 0; i < N.length(); i ++){
+	            if(i != 0 && i % 3 == 0){
+	                System.out.print(",");
+	            }
+	            System.out.print(N.charAt(i));
+	        }
+	        System.out.println();
+	        sc.close();
 	}
 }
