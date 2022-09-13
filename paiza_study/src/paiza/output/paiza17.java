2fa2217e2e4f885408d960390fa3991f5977e65a
“shostitch“ <stitchsho0325@gmail.com>
2022-09-13 22:53:27 +0900

    9/13学び

--- paiza_study/src/paiza/output/paiza17.java
+++ paiza_study/src/paiza/output/paiza17.java
@@ -10,6 +10,7 @@ public class paiza17 {
         Double N = sc.nextDouble();
         int M = sc.nextInt();
         System.out.println(String.format("%." + M + "f",N));
+        //String.format(中身を変数桁にする場合は文字列出力と同様に + や "" を複数利用して、整える)
         sc.close();
     }
 }
